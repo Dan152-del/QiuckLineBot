bb9a99a8b93888f8c8eaa4ccf1ac526cb2abe7ae
Dan Omelchuk <dan.omelchuk12@gmail.com>
2024-12-21 12:15:53 +0200

    Додав змінені файли

--- src/code.js
+++ src/code.js
@@ -1,9 +1,63 @@
 const { Telegraf } = require("telegraf");
-require(`dotenv`).config()
+require('dotenv').config();
+
 const bot = new Telegraf(process.env.token);
-bot.on("text", (ctx) =>{
-    console.log(ctx.message)
-}
-)
-bot.start((msg) => msg.reply("Welcome"))
-bot.launch(() =>  console.log("start"))
+
+bot.start((ctx) => {
+    ctx.reply("Оберіть континент:", {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: ' Європа', callback_data: 'Europe' }],
+                [{ text: ' Азія', callback_data: 'Asia' }],
+                [{ text: ' Африка', callback_data: 'Africa' }],
+            ]
+        }
+    });
+});
+
+bot.on("callback_query", (ctx) => {
+    const data = ctx.callbackQuery.data;
+
+    if (data === "Europe") {
+        ctx.reply("Виберіть країну в Європі:", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Франція', callback_data: 'France' }],
+                    [{ text: 'Німеччина', callback_data: 'Germany' }],
+                ]
+            }
+        });
+    } else if (data === "Asia") {
+        ctx.reply("Виберіть країну в Азії:", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Японія', callback_data: 'Japan' }],
+                    [{ text: 'Індія', callback_data: 'India' }],
+                ]
+            }
+        });
+    } else if (data === "Africa") {
+        ctx.reply("Виберіть країну в Африці:", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: ' Єгипет', callback_data: 'Egypt' }],
+                    [{ text: 'ПАР', callback_data: 'SouthAfrica' }],
+                ]
+            }
+        });
+    } else if (data === "France") {
+        ctx.reply("У Франції розмовляють французькою мовою.");
+    } else if (data === "Germany") {
+        ctx.reply("У Німеччині розмовляють німецькою мовою.");
+    } else if (data === "Japan") {
+        ctx.reply("У Японії розмовляють японською мовою.");
+    } else if (data === "India") {
+        ctx.reply("В Індії розмовляють гінді та англійською мовами.");
+    } else if (data === "Egypt") {
+        ctx.reply("В Єгипті розмовляють арабською мовою.");
+    } else if (data === "SouthAfrica") {
+        ctx.reply("У ПАР розмовляють англійською та африкаанс.");
+    }
+});
+
+bot.launch(() => console.log("Bot started"));
